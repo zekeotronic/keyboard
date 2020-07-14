2743f9f21fbf48e437c8ef2730e462b36856c5bc
Eric Ihle <zekeotronic@gmail.com>
2020-07-13 22:53:58 -0400

    final

---
+++ resources/js/main.js
@@ -0,0 +1,39 @@
+function playC() { 
+  new Audio('resources/audio/C.mp3').play(); 
+}
+function playCSharp() {
+  new Audio('resources/audio/C-sharp.mp3').play(); 
+}
+function playD() { 
+  new Audio('resources/audio/D.mp3').play(); 
+}
+function playDSharp() {
+  new Audio('resources/audio/D-sharp.mp3').play(); 
+}
+function playE() { 
+  new Audio('resources/audio/E.mp3').play(); 
+}
+function playF() { 
+  new Audio('resources/audio/F.mp3').play(); 
+}
+function playFSharp() { 
+  new Audio('resources/audio/F-sharp.mp3').play(); 
+}
+function playG() { 
+  new Audio('resources/audio/G.mp3').play(); 
+}
+function playGSharp() {
+  new Audio('resources/audio/G-sharp.mp3').play(); 
+}
+function playA() { 
+  new Audio('resources/audio/A.mp3').play(); 
+}
+function playASharp() {
+  new Audio('resources/audio/A-sharp.mp3').play(); 
+}
+function playB() { 
+  new Audio('resources/audio/B.mp3').play(); 
+}
+function playCOct() { 
+  new Audio('resources/audio/C-Oct.mp3').play(); 
+}
